14973d58c3276a3369d12b2914eee3d4883ea298
Garcia Alejandro <178788@ust-global.com>
2021-07-29 11:46:42 +0100

    Completed

--- practice/JsonYaml.cs
+++ practice/JsonYaml.cs
@@ -1,26 +1,23 @@
 ﻿
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
+
 using System;
-using System.Collections.Generic;
+
 using System.IO;
-using System.Linq;
-using System.Runtime.Serialization.Json;
-using System.Text;
-using System.Threading.Tasks;
+
 using YamlDotNet.RepresentationModel;
 
 
 namespace practice
 {
-    public class Greetings
+    public class JsonYaml
     {
         static void Main(string[] args)
         {
 
             StreamReader reader = new StreamReader("C:\\Users\\178788\\Desktop\\practice\\practice\\json1.json");
             string jsonString = reader.ReadToEnd();
-            //Console.WriteLine(jsonString);
+            
             var people = JsonConvert.DeserializeObject<Wrapper>(jsonString).Person;
             foreach (var person in people.People)
             {
@@ -31,7 +28,7 @@ namespace practice
             var yaml = new YamlStream();
             yaml.Load(reader2);
             var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
-            string key = string.Empty;
+            
 
             foreach (var entry in mapping.Children)
             {
