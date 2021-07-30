4ec6df2657f962211de579a483edb3d38e3a3b00
Garcia Alejandro <178788@ust-global.com>
2021-07-30 17:18:20 +0100

    completed

--- practice/logger.cs
+++ practice/logger.cs
@@ -9,6 +9,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using System.IO;
 using Microsoft.Extensions.DependencyInjection;
+using YamlDotNet.RepresentationModel;
+using Newtonsoft.Json;
 
 namespace practice
 
@@ -44,9 +46,36 @@ namespace practice
                 Log.Logger.Information(list[i].EmployeeDetails() + " " + list[i].displaySalary());
             }
 
-          
+            //JSON AND YAML PARSING HAPPENS HERE
+            StreamReader reader = new StreamReader("C:\\Users\\178788\\Desktop\\practice\\practice\\json1.json");
+            string jsonString = reader.ReadToEnd();
 
-            svc.Run();
+            var people = JsonConvert.DeserializeObject<Wrapper>(jsonString).Person;
+            foreach (var person in people.People)
+            {
+                Console.WriteLine("Name: {0} Age: {1} ", person.Value.name, person.Value.age);
+            }
+
+            var reader2 = new StreamReader("C:\\Users\\178788\\Desktop\\practice\\practice\\YamlPractice.yml");
+            var yaml = new YamlStream();
+            yaml.Load(reader2);
+            var mapping = (YamlMappingNode)yaml.Documents[0].RootNode;
+
+
+            foreach (var entry in mapping.Children)
+            {
+                var myKey = ((YamlScalarNode)entry.Key).Value;
+
+                Console.WriteLine("Key: {0} Value: {1}", myKey, entry.Value);
+                if (myKey != "turtle-doves")
+                {
+                    continue;
+                }
+            }
+
+
+
+                svc.Run();
         }
 
        
